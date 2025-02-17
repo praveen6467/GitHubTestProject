f49a554391b4d606298480cab614a2a9ffbf09f7
praveen6467 <praveen.giri0310@gmail.com>
2025-02-17 20:20:34 +0530

    Update first.java
    hi

--- CucumberFrame/src/test/java/POM/first.java
+++ CucumberFrame/src/test/java/POM/first.java
@@ -21,6 +21,7 @@ public class first {
 		System.out.println("sucefully login to git and gitHub ");
 		System.out.println("sucefully Connected to local and Remote Repositries //// ");
 		System.out.println("Changed in Remote GitHub");
+		System.out.println("Changed in Remote GitHub2");
 	}  
 	
 	@Test
